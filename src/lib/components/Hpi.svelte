23ceb4547a4206497c680669a5354f876761534e
sdicola <simone@property.xyz>
2024-08-02 10:00:06 +0100

    chore: adds debug data

--- src/lib/components/Hpi.svelte
+++ src/lib/components/Hpi.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import { fragment, graphql, type Hpi } from '$houdini';
+	import { onMount } from 'svelte';
 
 	export let hpi: Hpi;
+
+	let mounted = false;
 
 	$: hpiData = fragment(hpi, graphql(`
     fragment Hpi on Query @arguments(countries: {type: "[String!]!"}) {
@@ -18,10 +21,21 @@
     }
   `));
 
+	onMount(() => {
+		mounted = true;
+		console.log('Component mounted, hpi:', hpi);
+	});
+
 	$: console.log('hpiData updated:', $hpiData);
+	$: console.log('Mounted:', mounted);
+	$: console.log('Data available:', $hpiData.data?.uk_data_house_price_index?.length > 0);
 </script>
 
-{#if $hpiData.fetching}
+<pre>Debug: {JSON.stringify({ mounted, hpiData: $hpiData }, null, 2)}</pre>
+
+{#if !mounted}
+	<p>Initializing...</p>
+{:else if $hpiData.fetching}
 	<p>Loading data...</p>
 {:else if $hpiData.error}
 	<p>Error: {$hpiData.error.message}</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import Hpi from '$lib/components/Hpi.svelte';
 	import type { PageData } from '$houdini';
+	import { onMount } from 'svelte';
 
 	export let data: PageData;
 	$: ({ UkData } = data);
 
-	$: console.log(UkData);
+	$: console.log('PageData updated:', data);
+
+	onMount(() => {
+		console.log('Page component mounted');
+	});
 </script>
 
 <Hpi hpi={$UkData} />
